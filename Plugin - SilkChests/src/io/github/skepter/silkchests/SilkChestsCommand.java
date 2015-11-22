5aabd82b1559cee868681193c4fbeed04577c181
Skepter <skepter.misc@gmail.com>
2015-11-22 23:01:37 +0000

    creates silkchests command

--- Plugin - SilkChests/src/io/github/skepter/silkchests/SilkChestsCommand.java
+++ Plugin - SilkChests/src/io/github/skepter/silkchests/SilkChestsCommand.java
@@ -1,17 +1,32 @@
 package io.github.skepter.silkchests;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class SilkChestsCommand implements CommandExecutor {
 
+	String prefix = ChatColor.GOLD + "[" + ChatColor.YELLOW + "SilkChests" + ChatColor.GOLD + "] " + ChatColor.YELLOW;
+	
 	public SilkChestsCommand(Main main) {
 	}
 
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(command.getName().equalsIgnoreCase("silkchests") && sender.hasPermission("silkchests.admin")) {
+			if(args.length == 0) {
+				/* AA center code goes here, list of commands (reload) (config - views current setup) */
+				/* cmds to modify config ingame, make sure it updates config.yml file!! */
+				sender.sendMessage(prefix + "");
+			}
+			if(args.length > 0) {
+				switch(args[0]) {
+					
+				}
+			}
+			
+		}
 		return false;
 	}
 
